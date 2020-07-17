1e72820931f0343c6d571c8ef60c09e1278638f4
Suni1208 <skmy03@gmail.com>
2020-07-18 02:57:05 +0530

    controlled input

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -40,7 +40,7 @@ class Contact  extends Component {
     render() {
         return(
             <div className="container">
-                    <div className="row">
+                    <div  className="row">
                     <Breadcrumb>
                         <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
                         <BreadcrumbItem active>Contact</BreadcrumbItem>
